09f218a11836d5949425ba5f9c63f15da247afc5
Moisés Dias <123876519+moisesdn@users.noreply.github.com>
2026-01-13 08:15:06 -0300

    add KotlinTypes.kt with basic variable declarations and main function

---
+++ kotlin-project/src/main/kotlin/secao4/KotlinTypes.kt
@@ -0,0 +1,17 @@
+package br.com.moisesdias.secao4
+
+fun main() {
+    val myPhaser = "I'm a String"
+    val aString: String
+    val aDouble: Double
+    val aInt: Int = 199
+
+    val myDouble = 13.37
+    val myInt = 32
+    val myFloat = 2.5f
+
+    val aLong: Long = aInt.toLong()
+
+    println(aLong)
+
+}
